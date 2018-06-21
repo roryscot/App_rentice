f6df07c7885126cd12e63220eff58a0274d1a3cc
Rory Dahl <rsdah13@gmail.com>
2018-06-20 22:34:53 -0700

    Set up redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import {HomePage, NotFound} from './components';
+import { createStore } from "redux";
+import App_rentice from "./reducers";
 
 import './App.css';
+
+let store = createStore(App_rentice);
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import notes from "./notes";
+
+
+const App_rentice = combineReducers({
+  notes,
+})
+
+export default App_rentice;
