ea522767c1b199f7d9d9cd4200edda634769d1f5
Rory Dahl <rsdah13@gmail.com>
2018-06-21 10:24:32 -0700

    Defining actions and reducers

--- frontend/src/reducers/notes.js
+++ frontend/src/reducers/notes.js
@@ -3,9 +3,25 @@ const initialState = [
 ];
 
 
-export default function notes(state=initialState, action) {
-    switch (action.type) {
-        default:
-        return state;
-    }
+export default function handleNotes(state=initialState, action) {
+    let noteList = state.slice();
+
+  switch (action.type) {
+
+    case 'ADD_NOTE':
+      return [...state, {text: action.text}];
+
+    case 'UPDATE_NOTE':
+      let noteToUpdate = noteList[action.id]
+      noteToUpdate.text = action.text;
+      noteList.splice(action.id, 1, noteToUpdate);
+      return noteList;
+
+    case 'DELETE_NOTE':
+      noteList.splice(action.id, 1);
+      return noteList;
+
+    default:
+      return state;
+  }
 }
