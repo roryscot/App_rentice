b906ef8bc62052c5cb9ac06826784186f08a7b84
Rory Dahl <rsdah13@gmail.com>
2018-06-21 18:16:58 -0700

    Postgres setup

--- ApprenticeRoot/productionSettings.py
+++ ApprenticeRoot/productionSettings.py
@@ -1,14 +1,49 @@
 from .settings import *
+import environ
 
+#Read environment variables
+environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
+
+#defining which variables django-environ should look for
+env = environ.Env(
+    SECRET_KEY=str,
+    DEBUG=(bool, False),
+    DB_HOST=(str, '127.0.0.1'),
+    DB_NAME=str,
+    DB_USER=str,
+    DB_PASSWORD=str,
+    DB_PORT=int
+)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+# # Database
+# # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': env('DB_PORT'),
+#     }
+# }
+#
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 WEBPACK_LOADER = {
-    "DEFAULT": {
-        'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
-    }
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
+        }
 }
--- ApprenticeRoot/settings.py
+++ ApprenticeRoot/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'ApprenticeRoot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'App_rentice',
+        'USER': 'admin',
+        'PASSWORD': 'testtest',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
